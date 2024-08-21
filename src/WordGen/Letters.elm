619dfce8be1c791b38867f10fd1507966f817b89
Gideon <eignnx@gmail.com>
2024-08-21 15:55:56 -0400

    add letter database

--- src/WordGen/Letters.elm
+++ src/WordGen/Letters.elm
@@ -1,5 +1,6 @@
 module WordGen.Letters exposing (..)
 
+import Dict exposing (Dict)
 import Set
 
 
@@ -148,3 +149,180 @@ finalSets =
     , [ 'm', 'n', 'ŋ' ]
     , [ 's', 'ʃ', 'z', 'ʒ' ]
     ]
+
+
+type LetterArticulation
+    = Consonant
+        { manner : Manner
+        , place : Place
+        , voicing : Voicing
+        }
+    | Vowel
+
+
+type Manner
+    = Plosive
+    | Fricative
+    | Affricate
+    | Nasal
+    | Liquid
+    | Approximant ApproximantType
+    | SemiVowel
+    | Sibilant
+    | Trill
+    | Tap
+
+
+type ApproximantType
+    = Lateral
+    | Rhotic
+    | SemiVowel
+
+
+type Place
+    = Bilabial
+    | Labiodental
+    | Dental
+    | Alveolar
+    | Postalveolar
+    | Retroflex
+    | Palatal
+    | Velar
+    | Uvular
+    | Glottal
+
+
+type Voicing
+    = Voiced
+    | Voiceless
+
+
+charData : Dict Char LetterArticulation
+charData =
+    Dict.fromList
+        [ ( 't', Consonant { voicing = Voiceless, place = Alveolar, manner = Plosive } )
+        , ( 'd', Consonant { voicing = Voiced, place = Alveolar, manner = Plosive } )
+        , ( 'k', Consonant { voicing = Voiceless, place = Velar, manner = Plosive } )
+        , ( 'g', Consonant { voicing = Voiced, place = Velar, manner = Plosive } )
+        , ( 'p', Consonant { voicing = Voiceless, place = Bilabial, manner = Plosive } )
+        , ( 'b', Consonant { voicing = Voiced, place = Bilabial, manner = Plosive } )
+        , ( 'f', Consonant { voicing = Voiceless, place = Labiodental, manner = Fricative } )
+        , ( 'v', Consonant { voicing = Voiced, place = Labiodental, manner = Fricative } )
+        , ( 'θ', Consonant { voicing = Voiceless, place = Dental, manner = Fricative } )
+        , ( 'ð', Consonant { voicing = Voiced, place = Dental, manner = Fricative } )
+        , ( 'x', Consonant { voicing = Voiceless, place = Velar, manner = Fricative } )
+        , ( 'ɣ', Consonant { voicing = Voiced, place = Velar, manner = Fricative } )
+        , ( 's', Consonant { voicing = Voiceless, place = Alveolar, manner = Fricative } )
+        , ( 'z', Consonant { voicing = Voiced, place = Alveolar, manner = Fricative } )
+        , ( 'ʃ', Consonant { voicing = Voiceless, place = Postalveolar, manner = Fricative } )
+        , ( 'ʒ', Consonant { voicing = Voiced, place = Postalveolar, manner = Fricative } )
+        , ( 'm', Consonant { voicing = Voiced, place = Bilabial, manner = Nasal } )
+        , ( 'n', Consonant { voicing = Voiced, place = Alveolar, manner = Nasal } )
+        , ( 'ŋ', Consonant { voicing = Voiced, place = Velar, manner = Nasal } )
+        , ( 'l', Consonant { voicing = Voiced, place = Alveolar, manner = Approximant Lateral } )
+        , ( 'ɹ', Consonant { voicing = Voiced, place = Alveolar, manner = Approximant Rhotic } )
+        , ( 'r', Consonant { voicing = Voiced, place = Alveolar, manner = Trill } )
+        , ( 'ɾ', Consonant { voicing = Voiced, place = Alveolar, manner = Tap } )
+        , ( 'ʁ', Consonant { voicing = Voiced, place = Uvular, manner = Approximant Rhotic } )
+        , ( 'w', Consonant { voicing = Voiced, place = Bilabial, manner = Approximant SemiVowel } )
+        , ( 'j', Consonant { voicing = Voiced, place = Palatal, manner = Approximant SemiVowel } )
+        , ( 'ʔ', Consonant { voicing = Voiceless, place = Glottal, manner = Plosive } )
+        , ( 'a', Vowel )
+        , ( 'e', Vowel )
+        , ( 'i', Vowel )
+        , ( 'o', Vowel )
+        , ( 'u', Vowel )
+        , ( 'A', Vowel )
+        , ( 'E', Vowel )
+        , ( 'I', Vowel )
+        ]
+
+
+searchBy : (LetterArticulation -> Bool) -> List Char
+searchBy predicate =
+    Dict.filter
+        (\_ articulation ->
+            predicate articulation
+        )
+        charData
+        |> Dict.keys
+
+
+searchByMannar : Manner -> List Char
+searchByMannar manner =
+    searchBy
+        (\articulation ->
+            case articulation of
+                Consonant articulation ->
+                    articulation.manner == manner
+
+                Vowel ->
+                    False
+        )
+
+
+searchByPlace : Place -> List Char
+searchByPlace place =
+    searchBy
+        (\articulation ->
+            case articulation of
+                Consonant a ->
+                    a.place == place
+
+                Vowel ->
+                    False
+        )
+
+
+searchByVoicing : Voicing -> List Char
+searchByVoicing voicing =
+    searchBy
+        (\articulation ->
+            case articulation of
+                Consonant a ->
+                    a.voicing == voicing
+
+                Vowel ->
+                    False
+        )
+
+
+letterSatisfies : (LetterArticulation -> Bool) -> Char -> Bool
+letterSatisfies predicate letter =
+    case Dict.get letter charData of
+        Just articulation ->
+            predicate articulation
+
+        Nothing ->
+            False
+
+
+consonantSatisfies : ({ manner : Manner, place : Place, voicing : Voicing } -> Bool) -> Char -> Bool
+consonantSatisfies predicate letter =
+    case Dict.get letter charData of
+        Just (Consonant articulation) ->
+            predicate articulation
+
+        _ ->
+            False
+
+
+letterHasManner : Manner -> Char -> Bool
+letterHasManner manner letter =
+    consonantSatisfies
+        (\articulation -> articulation.manner == manner)
+        letter
+
+
+letterHasPlace : Place -> Char -> Bool
+letterHasPlace place letter =
+    consonantSatisfies
+        (\articulation -> articulation.place == place)
+        letter
+
+
+letterHasVoicing : Voicing -> Char -> Bool
+letterHasVoicing voicing letter =
+    consonantSatisfies
+        (\articulation -> articulation.voicing == voicing)
+        letter
