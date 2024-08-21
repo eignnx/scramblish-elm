74c6d13f300a223237b9134eca2a9592ac2594ec
Gideon <eignnx@gmail.com>
2024-08-21 15:05:55 -0400

    move letter set defs into own module

--- src/WordGen/Gen.elm
+++ src/WordGen/Gen.elm
@@ -9,152 +9,7 @@ import Html.Attributes exposing (class)
 import Random as R
 import Random.Extra as RX
 import Set
-
-
-allBaseConsonants =
-    []
-        ++ englishUnfriendlyConsonants
-        ++ List.concat (List.repeat englishFriendlyConsonantWeight englishFriendlyConsonants)
-
-
-englishUnfriendlyConsonants =
-    [ 'x' -- Pronounced like the "ch" in "Bach"
-    , 'ɣ' -- Pronounced like the "g" in "amigo" (some dialects).
-    , 'q' -- Pronounced like "k" but further back in the throat
-    , 'ʔ' -- Glottal stop, like the sound between the syllables in "uh-oh"
-    ]
-
-
-englishFriendlyConsonants =
-    [ 'p' -- Pronounced like "p" as in "pat"
-    , 'b' -- Pronounced like "b" as in "bat"
-    , 'f' -- Pronounced like "f" as in "fog"
-    , 'v' -- Pronounced like "v" as in "vase"
-    , 't' -- Pronounced like "t" as in "top"
-    , 'd' -- Pronounced like "d" as in "dog"
-    , 'k' -- Pronounced like "k" as in "kite"
-    , 'g' -- Pronounced like "g" as in "goat"
-    , 'm' -- Pronounced like "m" as in "mop"
-    , 'n' -- Pronounced like "n" as in "no"
-    , 'θ' -- Pronounced like "th" as in "thin"
-    , 'ð' -- Pronounced like "th" as in "this"
-    , 'ŋ' -- Pronounced like "ng" as in "sing"
-    ]
-
-
-englishFriendlyConsonantWeight =
-    7
-
-
-allConsonants =
-    (Set.fromList >> Set.toList)
-        (allBaseConsonants ++ allSibilants ++ allApproximants ++ List.concat finalSets)
-
-
-allVowels =
-    String.toList
-        "aaeeiioouu"
-        -- Capitalized vowels stand for language-specific vowels. Will be replaced
-        -- in the orthography stage.
-        ++ [ 'A', 'E', 'I' ]
-
-
-{-| Sibilants (from Latin: sībilāns : 'hissing') are fricative consonants of
-higher amplitude and pitch, made by directing a stream of air with the tongue
-towards the teeth.[1] Examples of sibilants are the consonants at the beginning
-of the English words sip, zip, ship, and genre. The symbols in the International
-Phonetic Alphabet used to denote the sibilant sounds in these words are,
-respectively, [s][z] [ʃ][ʒ]. Sibilants have a characteristically intense sound,
-which accounts for their paralinguistic use in getting one's attention (e.g.
-calling someone using "psst!" or quieting someone using "shhhh!").
-
-In the alveolar hissing sibilants [s] and [z], the back of the tongue forms a
-narrow channel (is grooved) to focus the stream of air more intensely, resulting
-in a high pitch. With the hushing sibilants (occasionally termed shibilants),
-such as English [ʃ], [tʃ], [ʒ], and [dʒ], the tongue is flatter, and the
-resulting pitch lower.[2][3]
-
-A broader category is stridents, which include more fricatives than sibilants
-such as uvulars. Sibilants are a higher pitched subset of the stridents. The
-English sibilants are:
-
-    /s, z, ʃ, ʒ, tʃ, dʒ/
-
-while the English stridents are:
-
-    /s, z, ʃ, ʒ, tʃ, dʒ, f, v/
-
-as /f/ and /v/ are stridents but not sibilants because they are lower in pitch.
-
-Be aware, some linguistics use the terms stridents and sibilants interchangeably
-to refer to the greater amplitude and pitch compared to other fricatives.[6]
-
-"Stridency" refers to the perceptual intensity of the sound of a sibilant
-consonant, or obstacle fricatives or affricates, which refers to the critical
-role of the teeth in producing the sound as an obstacle to the airstream.
-Non-sibilant fricatives and affricates produce their characteristic sound
-directly with the tongue or lips etc. and the place of contact in the mouth,
-without secondary involvement of the teeth.[citation needed]
-
-The characteristic intensity of sibilants means that small variations in tongue
-shape and position are perceivable, with the result that there are many sibilant
-types that contrast in various languages.
-
-(source: [Wikipedia](https://en.wikipedia.org/wiki/Sibilant))
-
--}
-allSibilants =
-    [ 's', 'ʃ', 'z', 'ʒ' ]
-
-
-{-| In linguistics, a liquid consonant or simply liquid is any of a class of
-consonants that consists of rhotics and voiced lateral approximants, which are
-also sometimes described as "R-like sounds" and "L-like sounds". The word liquid
-seems to be a calque of the Ancient Greek word ὑγρός (hygrós, transl. moist),
-initially used by grammarian Dionysius Thrax to describe Greek sonorants.
-
-Liquid consonants are more prone to be part of consonant clusters and of the
-syllable nucleus. Their third formants are generally non-predictable based on
-the first two formants. Another important feature is their complex articulation,
-which makes them a hard consonant class to study with precision and the last
-consonants to be produced by children during their phonological development.
-They are also more likely to undergo certain types of phonological changes such
-as assimilation, dissimilation and metathesis.
-
-Most languages have at least one liquid in their phonemic inventory. English has
-two, `/l/` and `/ɹ/`.
-
-(source: [Wikipedia](https://en.wikipedia.org/wiki/Liquid_consonant))
-
--}
-allApproximants =
-    [ 'l' -- Pronounced like "l" as in "lip"
-    , 'ɹ' -- Pronounced like "r" as in English "rat"
-    , 'r' -- Pronounced like "rr" as in Spanish "perro"
-    , 'ɾ' -- Pronounced like "r" as in Spanish "pero"
-    , 'ʁ' -- Pronounced like the French "r" in "rouge"
-    , 'w' -- Pronounced like "w" as in "wet"
-    , 'j' -- Pronounced like "y" as in "yes"
-    ]
-
-
-finalSets =
-    [ [ 'm', 'n' ]
-    , [ 's', 'k' ]
-    , [ 'c', 'ʃ' ]
-    , [ 'ð', 'θ' ]
-    , [ 'm', 'n', 'ŋ' ]
-    , [ 's', 'ʃ', 'z', 'ʒ' ]
-    ]
-
-
-type LetterClass
-    = C -- Consonant
-    | V -- Vowel
-    | S -- Sibilant
-    | A -- Approximant
-    | F -- Final
-    | Opt LetterClass -- An optional letter class
+import WordGen.Letters as L exposing (LetterClass(..))
 
 
 syllableStructureTemplates : List (List LetterClass)
@@ -203,17 +58,17 @@ type alias Language =
     , sibilants : List Char
     , approximants : List Char
     , finals : List Char
-    , syllableTemplate : List LetterClass
+    , syllableTemplate : List L.LetterClass
     }
 
 
 defaultLanguage : Language
 defaultLanguage =
-    { consonants = allBaseConsonants
-    , vowels = allVowels
-    , sibilants = allSibilants
-    , approximants = allApproximants
-    , finals = List.concat finalSets
+    { consonants = L.allBaseConsonants
+    , vowels = L.allVowels
+    , sibilants = L.allSibilants
+    , approximants = L.allApproximants
+    , finals = List.concat L.finalSets
     , syllableTemplate = [ C, V, C ]
     }
 
@@ -300,11 +155,11 @@ hasHardClusters lang prev syll =
                     [ ( [ 's', 'ʃ', 'v' ], [ 's', 'ʃ' ] )
                     , ( [ 'z', 'ʒ', 'f' ], [ 'z', 'ʒ' ] )
                     , ( [ 'd' ], [ 'ʃ' ] )
-                    , ( allApproximants, allApproximants )
+                    , ( L.allApproximants, L.allApproximants )
                     , ( [ 't', 'd' ], [ 't', 'd' ] )
                     , ( [ 'ð' ], [ 'ʃ' ] )
-                    , ( [ 'ɣ' ], allConsonants )
-                    , ( allConsonants, [ 'ɣ' ] )
+                    , ( [ 'ɣ' ], L.allConsonants |> Set.toList )
+                    , ( L.allConsonants |> Set.toList, [ 'ɣ' ] )
                     ]
             )
                 || hasHardClusters lang curr rest
@@ -394,12 +249,12 @@ randomLanguage =
             RX.choice [] syllableStructureTemplates
 
         consonantsR =
-            RX.subsetMinMax 4 (List.length (englishUnfriendlyConsonants ++ englishFriendlyConsonants) // 5 * 4) allBaseConsonants
+            RX.subsetMinMax 4 (Set.size L.allConsonants // 5 * 4) (Set.toList L.allConsonants)
                 |> R.map (Set.fromList >> Set.toList)
 
         vowelsR =
-            RX.lowerWeightedRange (\x -> sqrt x) 2 (List.length allVowels // 5 * 4)
-                |> R.andThen (\n -> RX.subsetN n allVowels)
+            RX.lowerWeightedRange (\x -> sqrt x) 2 (List.length L.allVowels // 5 * 4)
+                |> R.andThen (\n -> RX.subsetN n L.allVowels)
                 |> R.map (Set.fromList >> Set.toList)
     in
     syllableTemplateR
@@ -431,15 +286,15 @@ randomLanguage =
 
                     sibilantsR : R.Generator (List Char)
                     sibilantsR =
-                        RX.subsetMin 1 allSibilants
+                        RX.subsetMin 1 L.allSibilants
                             |> ifClassIsRelevant S
 
                     approximantsR =
-                        RX.subsetMin 1 allApproximants
+                        RX.subsetMin 1 L.allApproximants
                             |> ifClassIsRelevant A
 
                     finalsR =
-                        RX.subsetMinMax 1 2 finalSets
+                        RX.subsetMinMax 1 2 L.finalSets
                             |> R.map (List.concat >> Set.fromList >> Set.toList)
                             |> ifClassIsRelevant F
                 in
