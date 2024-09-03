8e0fdcacf5d42ed96482d4c19875f8fbff5aab86
Gideon <eignnx@gmail.com>
2024-09-03 01:23:21 -0400

    increase size of user translation delete buttons

--- src/Main.elm
+++ src/Main.elm
@@ -624,7 +624,7 @@ viewUserTranslations wordStats userTranslations =
                         , onClick
                             (DeleteTranslationPair { eng = eng, scr = scr })
                         ]
-                        [ text "x" ]
+                        [ text "🗙" ]
                     ]
                 ]
     in
